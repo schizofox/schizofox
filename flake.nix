fa72c0341f9aaab18f56fd332eacf866ab650354
Max <max@privatevoid.net>
2023-08-29 20:04:52 +0200

    inputs: flake-parts/nixpkgs-lib -> nixpkgs

--- flake.nix
+++ flake.nix
@@ -3,7 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
     nixpak = {
       url = "github:nixpak/nixpak";
       inputs.nixpkgs.follows = "nixpkgs";
