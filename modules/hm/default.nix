856aa19ce502a92066f42fdab750c916817629a8
Gerg-L <GregLeyda@proton.me>
2023-08-29 22:39:49 -0400

    Fix: Switched to wrapper and renamed binary

--- modules/hm/default.nix
+++ modules/hm/default.nix
@@ -17,7 +17,7 @@ self: {
   mozillaConfigPath =
     if isDarwin
     then "Library/Application Support/Mozilla"
-    else ".mozilla";
+    else "${config.home.homeDirectory}/.mozilla";
 
   firefoxConfigPath =
     if isDarwin
@@ -284,7 +284,32 @@ in {
     };
 
     home.packages = let
-      pkg = import ./firefox {inherit pkgs cfg lib self;};
+      _pkg = import ./firefox {inherit pkgs cfg lib self;};
+        logo = builtins.fetchurl {
+          url = "https://raw.githubusercontent.com/schizofox/assets/main/logo/logo.png";
+          sha256 = "1wjzivdmppbzrwdxhza5dzzljl3z59vfgggxim9xjb2rzr0wqyyf";
+        };
+    desktopItem = pkgs.makeDesktopItem
+      {
+        name = "Schizofox";
+        desktopName = "Schizofox";
+        genericName = "Web Browser";
+        exec = "schizofox %U";
+        icon = "${logo}";
+        terminal = false;
+        categories = ["Application" "Network" "WebBrowser"];
+        mimeTypes = ["text/html" "text/xml"];
+      };
+      pkg = pkgs.symlinkJoin {
+        name = "Schizofox";
+        paths = [_pkg desktopItem];
+        nativeBuildInputs = [pkgs.makeBinaryWrapper];
+        postBuild = ''
+          makeWrapper $out/bin/firefox $out/bin/schizofox \
+            --add-flags '-Profile ${profilesPath}/schizo.default'
+          find $out/share/applications -type l ! -name 'Schizofox.desktop' -delete
+        '';
+      };
     in
       if cfg.security.sandbox
       then [
@@ -391,21 +416,5 @@ in {
         .env
       ]
       else [pkg];
-    xdg.desktopEntries = {
-      firefox = let
-        logo = builtins.fetchurl {
-          url = "https://raw.githubusercontent.com/schizofox/assets/main/logo/logo.png";
-          sha256 = "1wjzivdmppbzrwdxhza5dzzljl3z59vfgggxim9xjb2rzr0wqyyf";
-        };
-      in {
-        name = "Schizofox";
-        genericName = "Web Browser";
-        exec = "firefox -Profile ${profilesPath}/schizo.default %U";
-        icon = "${logo}";
-        terminal = false;
-        categories = ["Application" "Network" "WebBrowser"];
-        mimeType = ["text/html" "text/xml"];
-      };
-    };
   };
 }
