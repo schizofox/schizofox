22f1bec0a78388fa54fd61e1a92906666489d5e7
NotAShelf <raf@notashelf.dev>
2023-09-18 14:55:29 +0300

    modules/hm: bump default firefox package to firefox-esr-115-unwrapped
    firefox-esr-102 has been deprecated as of https://github.com/NixOS/nixpkgs/pull/254273

--- modules/hm/default.nix
+++ modules/hm/default.nix
@@ -48,7 +48,7 @@ in {
   options.programs.schizofox = {
     enable = mkEnableOption (mdDoc "Schizophrenic Firefox ESR setup for the delusional");
     package = mkPackageOption pkgs "firefox-esr-115-unwrapped" {
-      example = "firefox-esr-115-unwrapped";
+      example = "firefox-esr-unwrapped";
     };
 
     theme = {
