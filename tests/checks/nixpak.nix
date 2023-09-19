96a6e90c3552d9f50afd10f2432331206e4aca86
NotAShelf <raf@notashelf.dev>
2023-09-19 11:01:46 +0300

    flake/checks: add nixpak test

--- tests/checks/nixpak.nix
+++ tests/checks/nixpak.nix
@@ -10,14 +10,17 @@ nixosTest {
   nodes.machine = {
     imports = [
       inputs.home-manager.nixosModules.home-manager
-      ./profiles/test-setup.nix
+      ../profiles/test-setup.nix
     ];
     home-manager.sharedModules = [
       homeManagerModules.schizofox
     ];
 
     home-manager.users.test = {
-      programs.schizofox.enable = true;
+      programs.schizofox = {
+        enable = true;
+        security.sandbox = true;
+      };
     };
   };
 
