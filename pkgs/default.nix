3c42b0432b5479e40e0337f42ac79fb3b79d78be
NotAShelf <raf@notashelf.dev>
2023-10-06 23:42:12 +0300

    pkgs: move custom userChrome and userContent into packages

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -5,6 +5,10 @@ _: {
     packages = {
       # Extensions
       darkreader = pkgs.callPackage ./darkreader.nix {};
+
+      # Simplefox
+      userChrome = pkgs.callPackage ./simplefox/userChrome.nix {};
+      userContent = pkgs.callPackage ./simplefox/userContent.nix {};
     };
   };
 }
