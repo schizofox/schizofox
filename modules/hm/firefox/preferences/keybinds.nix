ba1012450b3968d749bb599f8e057393617a0d9c
NotAShelf <raf@notashelf.dev>
2023-12-09 22:56:14 +0300

    preferences: init keybinds section

---
+++ modules/hm/firefox/preferences/keybinds.nix
@@ -0,0 +1,10 @@
+{
+  # Keybindings
+  "ui.key.textcontrol.prefer_native_key_bindings_over_builtin_shortcut_key_definitions" = true;
+  # https://searchcode.com/codesearch/view/26755902/
+  # default - 17 (ctrl)
+  "ui.key.accelKey" = 17;
+  # default - 18 (alt)
+  "ui.key.menuAccessKey" = 18;
+  "ui.key.menuAccessKeyFocuses" = true;
+}
