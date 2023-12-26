a4c88057df85a1e3127e1fdb0e247c63b2fccf20
mrtnvgr <root@unixis.fun>
2023-12-26 11:44:05 +0500

    refactor: restructure some options

--- modules/hm/options/theme.nix
+++ modules/hm/options/theme.nix
@@ -9,51 +9,35 @@
   cfg = config.programs.schizofox;
 in {
   options.programs.schizofox.theme = {
+    colors = {
+      background-darker = mkOption {
+        type = types.str;
+        example = "181825";
+        default = "181825";
+        description = "Darker background color";
+      };
+
+      background = mkOption {
+        type = types.str;
+        example = "1e1e2e";
+        default = "1e1e2e";
+        description = "Background color";
+      };
+
+      foreground = mkOption {
+        type = types.str;
+        example = "cdd6f4";
+        default = "cdd6f4";
+        description = "Text color";
+      };
+    };
+
     font = mkOption {
       type = types.str;
       example = "Lato";
       default = "Lexend";
       description = "Default firefox font";
     };
-
-    background-darker = mkOption {
-      type = types.str;
-      example = "181825";
-      default = "181825";
-      description = "Darker background color";
-    };
-
-    background = mkOption {
-      type = types.str;
-      example = "1e1e2e";
-      default = "1e1e2e";
-      description = "Dark reader background color";
-    };
-
-    foreground = mkOption {
-      type = types.str;
-      example = "cdd6f4";
-      default = "cdd6f4";
-      description = "Dark reader text color";
-    };
-
-    # simple fox options
-    simplefox = {
-      enable = mkEnableOption ''
-        A Userstyle theme for Firefox minimalist and Keyboard centered.
-      '';
-
-      showUrlBar = mkEnableOption ''
-        Show the URL bar on hover.
-      '';
-    };
-
-    # TODO: patchDefaultColors bool option
-    darkreader.enable =
-      mkEnableOption ''
-        Dark mode on all sites (patched to match overall theme)
-      ''
-      // {default = true;}; # no escape
 
     extraUserChrome = mkOption {
       type = types.str;
