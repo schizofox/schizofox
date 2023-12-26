1c9eba0f66a3d83d627457e70d78bb01337f8a21
mrtnvgr <root@unixis.fun>
2023-12-26 12:06:04 +0500

    format

--- modules/hm/options/renamed.nix
+++ modules/hm/options/renamed.nix
@@ -6,11 +6,11 @@ in {
 
     (mkRenamedOptionModule ["programs" "schizofox" "theme" "extraCss"] ["programs" "schizofox" "theme" "extraUserChrome"])
     (mkRenamedOptionModule ["programs" "schizofox" "theme" "extraCssContent"] ["programs" "schizofox" "theme" "extraUserContent"])
-    (mkRenamedOptionModule ["programs" "schizofox" "theme" "background-darker"] ["programs" "schizofox" "theme" "colors" "background-darker"] )
-    (mkRenamedOptionModule ["programs" "schizofox" "theme" "background"] ["programs" "schizofox" "theme" "colors" "background"] )
-    (mkRenamedOptionModule ["programs" "schizofox" "theme" "foreground"] ["programs" "schizofox" "theme" "colors" "foreground"] )
+    (mkRenamedOptionModule ["programs" "schizofox" "theme" "background-darker"] ["programs" "schizofox" "theme" "colors" "background-darker"])
+    (mkRenamedOptionModule ["programs" "schizofox" "theme" "background"] ["programs" "schizofox" "theme" "colors" "background"])
+    (mkRenamedOptionModule ["programs" "schizofox" "theme" "foreground"] ["programs" "schizofox" "theme" "colors" "foreground"])
 
-    (mkRenamedOptionModule ["programs" "schizofox" "theme" "simplefox"] ["programs" "schizofox" "extensions" "simplefox"] )
-    (mkRenamedOptionModule ["programs" "schizofox" "theme" "darkreader"] ["programs" "schizofox" "extensions" "darkreader"] )
+    (mkRenamedOptionModule ["programs" "schizofox" "theme" "simplefox"] ["programs" "schizofox" "extensions" "simplefox"])
+    (mkRenamedOptionModule ["programs" "schizofox" "theme" "darkreader"] ["programs" "schizofox" "extensions" "darkreader"])
   ];
 }
