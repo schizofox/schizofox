79d80fa1293538c6f6be4706e1feda4fb785fbb6
mrtnvgr <root@unixis.fun>
2023-12-30 16:56:44 +0500

    add toolbar bg color (trying to add support for default firefox UI)

--- modules/hm/firefox/userChrome.nix
+++ modules/hm/firefox/userChrome.nix
@@ -4,7 +4,7 @@
   pkgs,
   self,
 }: let
-  inherit (cfg.theme.colors) background-darker background primary border;
+  inherit (cfg.theme.colors) background-darker background foreground primary border;
   userChrome = self.packages.${pkgs.stdenv.hostPlatform.system}.userChrome.override {
     backgroundDarker = background-darker;
     inherit background border;
@@ -12,6 +12,30 @@
   };
 in ''
   ${lib.optionalString cfg.extensions.simplefox.enable (builtins.readFile userChrome)}
+
+  :root {
+    --toolbar-bgcolor: #${background} !important;
+  }
+
+  #navigator-toolbox {
+    background: #${background-darker} !important;
+  }
+
+  tab {
+    color: #${foreground} !important;
+  }
+
+  .toolbarbutton-1 {
+    color: #${foreground} !important;
+  }
+
+  #urlbar {
+    color: #${foreground} !important;
+  }
+
+  #urlbar-background {
+    background: #${background-darker} !important;
+  }
 
   menubar > menu[open] {
     border-bottom-color: #${primary} !important;
