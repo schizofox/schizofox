735e80295272739531dbefc632ac039777740ac6
NotAShelf <raf@notashelf.dev>
2024-06-15 13:45:04 +0300

    options/extensions: rework default extension behaviour
    The new behaviour dictates
    so that default extensions set is immutable, and can only be toggled. If user wishes to install additional
    extensions, then they would go into extraExtensions set which can also toggled with an option.

--- modules/home-manager/firefox/extensions/default.nix
+++ modules/home-manager/firefox/extensions/default.nix
@@ -1,22 +1,29 @@
 {
-  lib,
-  pkgs,
   self,
+  pkgs,
+  lib,
   cfg,
   ...
 }: let
-  ext = cfg.extensions;
+  inherit (lib.attrsets) mapAttrs optionalAttrs;
   inherit (self.packages.${pkgs.stdenv.hostPlatform.system}) darkreader;
+
   reader = darkreader.override {
     inherit (cfg.theme.colors) background foreground;
   };
 
-  # TODO: move to this to its own custom lib
-  # i.e inputs.schizofox.schizolib.mkForceInstalled
-  mkForceInstalled = builtins.mapAttrs (_: cfg: {installation_mode = "force_installed";} // cfg);
+  ext = cfg.extensions;
+
+  # TODO: move the mkForceInstalled function to a local library output defined by the flake.
+  # Ideally we would do something like self.lib.mkForceInstalled.
+  mkForceInstalled = mapAttrs (_: cfg: {installation_mode = "force_installed";} // cfg);
+
+  # HACK: We shouldn't really abuse optionalAttrs like this here, but use mkMerge.
+  # For some reason though, mkMerge fools mkForceInstalled and causes it to think
+  # we are passing a string (what?), so we use optionalAttrs instead.
   addons =
-    ext.defaultExtensions
-    // lib.optionalAttrs ext.darkreader.enable {"addon@darkreader.org".install_url = "file://${reader}/release/darkreader-firefox.xpi";}
-    // lib.optionalAttrs (ext.extraExtensions != {}) ext.extraExtensions;
+    optionalAttrs ext.enableDefaultExtensions ext.defaultExtensions
+    // optionalAttrs ext.enableExtraExtensions ext.extraExtensions
+    // optionalAttrs ext.darkreader.enable {"addon@darkreader.org".install_url = "file://${reader}/release/darkreader-firefox.xpi";};
 in
   mkForceInstalled addons
--- modules/home-manager/options/extensions.nix
+++ modules/home-manager/options/extensions.nix
@@ -1,45 +1,57 @@
 {lib, ...}: let
   inherit (lib.options) mkOption literalExpression mkEnableOption;
-  inherit (lib.types) attrs;
+  inherit (lib.types) attrs bool;
 in {
   options.programs.schizofox.extensions = {
+    enableDefaultExtensions = mkEnableOption ''
+      default extensions provided by Schizofox.
+    '';
+
+    enableExtraExtensions = mkEnableOption ''
+      extra extensions added to the user configuration.
+    '';
+
     simplefox = {
       enable = mkEnableOption ''
-        A Userstyle theme for Firefox minimalist and Keyboard centered.
+        Simplefox, an userstyle theme for Firefox minimalist and Keyboard centered.
       '';
 
-      showUrlBar = mkEnableOption ''
-        Show the URL bar on hover.
-      '';
+      showUrlBar = mkOption {
+        type = bool;
+        default = false;
+        description = ''
+          Whether to show the URL bar on hover.
+        '';
+      };
     };
 
-    # TODO: patchDefaultColors bool option
-    darkreader.enable =
-      mkEnableOption ''
-        Dark mode on all sites (patched to match overall theme)
-      ''
-      // {default = true;}; # no escape
+    darkreader.enable = mkEnableOption ''
+      Dark mode on all websites with a patched version of DarkReader
+      to conform to your Schizofox theming options.
+
+      ::: {.note}
+      This contributes to your browser fingerprinting, and is therefore
+      disabled by default
+      :::
+    '';
 
     defaultExtensions = mkOption {
+      readOnly = true;
+      internal = true;
       type = attrs;
       default = {
         "uBlock0@raymondhill.net".install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-        "{36bdf805-c6f2-4f41-94d2-9b646342c1dc}".install_url = "https://addons.mozilla.org/firefox/downloads/latest/export-cookies-txt/latest.xpi";
-        "{74145f27-f039-47ce-a470-a662b129930a}".install_url = "https://addons.mozilla.org/firefox/downloads/latest/clearurls/latest.xpi";
-        "{b86e4813-687a-43e6-ab65-0bde4ab75758}".install_url = "https://addons.mozilla.org/firefox/downloads/latest/localcdn-fork-of-decentraleyes/latest.xpi";
-        "DontFuckWithPaste@raim.ist".install_url = "https://addons.mozilla.org/firefox/downloads/latest/don-t-fuck-with-paste/latest.xpi";
-        "{531906d3-e22f-4a6c-a102-8057b88a1a63}".install_url = "https://addons.mozilla.org/firefox/downloads/latest/single-file/latest.xpi";
-        "{c607c8df-14a7-4f28-894f-29e8722976af}".install_url = "https://addons.mozilla.org/firefox/downloads/latest/temporary-containers/latest.xpi";
-        "skipredirect@sblask".install_url = "https://addons.mozilla.org/firefox/downloads/latest/skip-redirect/latest.xpi";
-        "smart-referer@meh.paranoid.pk".install_url = "https://addons.mozilla.org/firefox/downloads/latest/smart-referer/latest.xpi";
-        "7esoorv3@alefvanoon.anonaddy.me".install_url = "https://addons.mozilla.org/firefox/downloads/latest/libredirect/latest.xpi";
       };
 
-      example = literalExpression "{}";
-
       description = ''
-        A set of addons that will be installed by default. Can be set to `{}` to avoid installing
-        any of the default addons.
+        A set of addons that will be installed by default. Can be set to `{}` to avoid
+        installing any of the default addons. The behaviour of `defaultExtensions` can
+        be disabled by toggling the {option}`enableDefaultExtensions` option under
+        {option}`programs.schizofox.extensions`.
+
+        ::: {.note}
+        Schizofox only provides uBlock Origin out of the box.
+        :::
       '';
     };
 
@@ -47,12 +59,27 @@ in {
       type = attrs;
       default = {};
       description = ''
-        Extra extensions that will be installed in addition to default extensions. Will be merged
-        with the attribute set provided by `defaultExtensions`.
+        Extra extensions that will be installed in addition to default extensions.
+        Any extensions that are added here will be merged with the attribute set
+        provided by `defaultExtensions`.
+
+        ::: {.warning}
+        Remember to keep your extensions list brief, as unique extension combinations
+        will also contribute to your fingerprinting
+        :::
       '';
+
       example = literalExpression ''
         {
-          "webextension@metamask.io".install_url = "https://addons.mozilla.org/firefox/downloads/latest/ether-metamask/latest.xpi";
+          "{36bdf805-c6f2-4f41-94d2-9b646342c1dc}".install_url = "https://addons.mozilla.org/firefox/downloads/latest/export-cookies-txt/latest.xpi";
+          "{74145f27-f039-47ce-a470-a662b129930a}".install_url = "https://addons.mozilla.org/firefox/downloads/latest/clearurls/latest.xpi";
+          "{b86e4813-687a-43e6-ab65-0bde4ab75758}".install_url = "https://addons.mozilla.org/firefox/downloads/latest/localcdn-fork-of-decentraleyes/latest.xpi";
+          "DontFuckWithPaste@raim.ist".install_url = "https://addons.mozilla.org/firefox/downloads/latest/don-t-fuck-with-paste/latest.xpi";
+          "{531906d3-e22f-4a6c-a102-8057b88a1a63}".install_url = "https://addons.mozilla.org/firefox/downloads/latest/single-file/latest.xpi";
+          "{c607c8df-14a7-4f28-894f-29e8722976af}".install_url = "https://addons.mozilla.org/firefox/downloads/latest/temporary-containers/latest.xpi";
+          "skipredirect@sblask".install_url = "https://addons.mozilla.org/firefox/downloads/latest/skip-redirect/latest.xpi";
+          "smart-referer@meh.paranoid.pk".install_url = "https://addons.mozilla.org/firefox/downloads/latest/smart-referer/latest.xpi";
+          "7esoorv3@alefvanoon.anonaddy.me".install_url = "https://addons.mozilla.org/firefox/downloads/latest/libredirect/latest.xpi";
         }
       '';
     };
