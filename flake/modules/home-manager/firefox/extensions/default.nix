c3948a7a12989dc9b4b51f1d4b3ecf83e08d9d60
diniamo <diniamo53@gmail.com>
2024-06-17 22:37:58 +0200

    feat: filter out null addons

--- flake/modules/home-manager/firefox/extensions/default.nix
+++ flake/modules/home-manager/firefox/extensions/default.nix
@@ -5,7 +5,7 @@
   cfg,
   ...
 }: let
-  inherit (lib.attrsets) mapAttrs optionalAttrs;
+  inherit (lib.attrsets) mapAttrs optionalAttrs filterAttrs;
   inherit (self.packages.${pkgs.stdenv.hostPlatform.system}) darkreader;
 
   reader = darkreader.override {
@@ -14,16 +14,15 @@
 
   ext = cfg.extensions;
 
-  # TODO: move the mkForceInstalled function to a local library output defined by the flake.
+  # TODO: move these functions to a local library output defined by the flake.
   # Ideally we would do something like self.lib.mkForceInstalled.
+  removeNullAttrs = filterAttrs (_: value: value != null);
   mkForceInstalled = mapAttrs (_: cfg: {installation_mode = "force_installed";} // cfg);
 
-  # HACK: We shouldn't really abuse optionalAttrs like this here, but use mkMerge.
-  # For some reason though, mkMerge fools mkForceInstalled and causes it to think
-  # we are passing a string (what?), so we use optionalAttrs instead.
+  # HACK: Convert this to use mkMerge once the whole project uses the module system.
   addons =
     optionalAttrs ext.enableDefaultExtensions ext.defaultExtensions
     // optionalAttrs ext.enableExtraExtensions ext.extraExtensions
     // optionalAttrs ext.darkreader.enable {"addon@darkreader.org".install_url = "file://${reader}/release/darkreader-firefox.xpi";};
 in
-  mkForceInstalled addons
+  mkForceInstalled (removeNullAttrs addons)
