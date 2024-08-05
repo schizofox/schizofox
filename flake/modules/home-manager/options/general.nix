9ec386f32ee695b6c2792542a5e76dc5e6444d21
NotAShelf <raf@notashelf.dev>
2024-08-05 23:24:31 +0300

    modules/firefox: allow overriding wrapperArgs; add schizofox.misc.speechSynthesisSupport
    programs.schizofox.extraWrapperArgs will allow the user to pass a freeform attribute set to the wrapper as is, in order to merge additional arguments to the initial wrapper. This is especially useful when wrapFirefox has new options that *do not* have options in Schizofox module yet. Do note that this is extremely experimental and very implicit. Expect breakage, and use with great care.

--- flake/modules/home-manager/options/general.nix
+++ flake/modules/home-manager/options/general.nix
@@ -4,8 +4,8 @@
   ...
 }: let
   inherit (lib.options) mkEnableOption mkPackageOption mkOption literalExpression;
-  inherit (lib.types) attrsOf;
-  jsonFormat = pkgs.formats.json {};
+  inherit (lib.types) submodule attrsOf listOf package anything;
+  jsonType = (pkgs.formats.json {}).tyepe;
 in {
   options.programs.schizofox = {
     enable = mkEnableOption "Schizofox";
@@ -14,9 +14,12 @@ in {
     };
 
     settings = mkOption {
-      type = attrsOf (jsonFormat.type
+      type = attrsOf (jsonType
         // {
-          description = "Firefox preference (int, bool, string, and also attrs, list, float as a JSON string)";
+          description = ''
+            Firefox preference (int, bool, string, and also attrs, list, float
+            as a JSON string)
+          '';
         });
       default = {};
       example = literalExpression ''
@@ -37,9 +40,56 @@ in {
         Attribute set of Firefox preferences.
 
         Firefox only supports int, bool, and string types for
-        preferences, but `programs.schizofox.settings will
+        preferences, but {option}`programs.schizofox.settings` will
         automatically convert all other JSON-compatible values
         into strings.
+      '';
+    };
+
+    extraWrapperArgs = mkOption {
+      type = submodule {
+        freeformType = attrsOf anything;
+
+        # Freeform options are initially not validated by a schema
+        # so we are able to pass anything we wish to them. Below
+        # options are the ones we **want** type-checked so that the
+        # end-user does not hit obscure errors due to type mismatches.
+        options = {
+          nixExtensions = mkOption {
+            type = listOf package;
+            default = [];
+            example = ''
+              [
+                (fetchFirefoxAddon {
+                  name = "ublock"; # Has to be unique!
+                  url = "https://addons.mozilla.org/firefox/downloads/file/3679754/ublock_origin-1.31.0-an+fx.xpi";
+                  hash = "sha256-2e73AbmYZlZXCP5ptYVcFjQYdjDp4iPoEPEOSCVF5sA=";
+                })
+              ]
+            '';
+
+            description = ''
+              Additional Firefox extensions to be passed to wrapFirefox.
+
+              ::: {.warning}
+              This option can *only* be used with an ESR build of Firefox.
+              :::
+            '';
+          };
+        };
+      };
+
+      description = ''
+        Additional wrapper arguments to be passed to pkgs.wrapFirefox.
+
+        Do keep in mind that this is a freeform module, which means you
+        are able to pass virtually any attribute that you wish to and it
+        will be passed to wrapFirefox verbatim.
+
+        This can be used to set options that do not have configuration
+        interfaces in the Schizofox module, but it must also be used with
+        great care.
+
       '';
     };
   };
