55ef2de7392920a9a5522db5971093f4bbd1613f
NotAShelf <raf@notashelf.dev>
2025-01-05 07:15:25 +0300

    meta: modernize nix2 compat interface

--- default.nix
+++ default.nix
@@ -6,7 +6,7 @@
       nodeName = lock.nodes.root.inputs.flake-compat;
     in
       fetchTarball {
-        url = lock.nodes.${nodeName}.locked.url;
+        url = lock.nodes.${nodeName}.locked.url or "https://github.com/edolstra/flake-compat/archive/${lock.nodes.${nodeName}.locked.rev}.tar.gz";
         sha256 = lock.nodes.${nodeName}.locked.narHash;
       }
   )
