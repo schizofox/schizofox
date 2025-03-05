59f5ed60d5bd2a61b4f54b077fb439db1b98ef7b
Ben Mayer <info@leocodes.net>
2025-03-05 12:46:13 +0100

    modules/home-manager: add option to customize .mozilla folder location

--- flake/modules/home-manager/options/misc.nix
+++ flake/modules/home-manager/options/misc.nix
@@ -1,6 +1,12 @@
-{lib, ...}: let
+{
+  lib,
+  config,
+  ...
+}: let
   inherit (lib.options) mkOption mkEnableOption literalExpression;
   inherit (lib.types) enum listOf attrs bool str nullOr;
+
+  cfg = config.programs.schizofox;
 in {
   options.programs.schizofox.misc = {
     startPageURL = mkOption {
@@ -79,5 +85,42 @@ in {
       // {default = true;};
 
     showHomeButton = mkEnableOption "displaying Home Button in the toolbar";
+
+    customMozillaFolder = {
+      enable = mkEnableOption ''
+        a custom .mozilla folder for storing browser data.
+
+        You may want this so it doesn't clash with other firefox installations.
+      '';
+      path = let
+        # the extra check code has been taken from github.com/nix-community/disko
+        # it was, as far as I can tell written by Lassulus <https://github.com/lassulus>
+        # the project is licensed under MIT License,
+        # which can be found at https://github.com/nix-community/disko/blob/master/LICENSE
+        # Copyright (c) 2018, 2019, 2022–2024 Nix community projects
+        check = x: let
+          # The filter is used to normalize paths, i.e. to remove duplicated and
+          # trailing slashes.  It also removes leading slashes, thus we have to
+          # check for "/" explicitly below.
+          xs = lib.filter (s: lib.stringLength s > 0) (lib.splitString "/" x);
+        in
+          lib.isString x && (x == "/" || lib.length xs > 0) && lib.substring 0 1 x == "/";
+      in
+        mkOption {
+          type = lib.types.addCheck lib.types.str check;
+          default = "/.schizofox/mozilla";
+          example = "/.local/share/schizofox/mozilla";
+          description = "A relative path, from the users home directory, to the folder that will contain schizofoxes data and configuration";
+        };
+    };
   };
+  config.assertions = [
+    {
+      assertion = !(!cfg.security.sandbox.enable && cfg.misc.customMozillaFolder.enable);
+      message = ''
+        To use a custom .mozilla folder sanboxing must be enabled.
+        Enable sandboxing using 'programs.schizofox.security.sandbox.enable'
+      '';
+    }
+  ];
 }
