66c8f0ed12f65cc584066614338d93f975218198
NotAShelf <raf@notashelf.dev>
2026-09-19 16:16:10 +0300

    nix: switch flake-compat to Lix's fork
    Signed-off-by: NotAShelf <raf@notashelf.dev>
    Change-Id: I34dee5b27a7bc47a4be7aa9bfd5257686a6a6964

--- default.nix
+++ default.nix
@@ -1,15 +1,28 @@
-(
-  import
-  (
-    let
-      lock = builtins.fromJSON (builtins.readFile ./flake.lock);
-      nodeName = lock.nodes.root.inputs.flake-compat;
-    in
+let
+  inherit (builtins) fromJSON readFile fetchTarball;
+  locked = fromJSON (readFile ./flake.lock);
+
+  fetchInput = inputName: let
+    node = locked.nodes.${inputName}.locked or (throw "Input '${inputName}' missing from flake.lock");
+  in
+    if node.type == "github"
+    then
       fetchTarball {
-        url = lock.nodes.${nodeName}.locked.url or "https://github.com/edolstra/flake-compat/archive/${lock.nodes.${nodeName}.locked.rev}.tar.gz";
-        sha256 = lock.nodes.${nodeName}.locked.narHash;
+        url = "https://github.com/${node.owner}/${node.repo}/archive/${node.rev}.tar.gz";
+        sha256 = node.narHash;
       }
-  )
-  {src = ./.;}
-)
-.defaultNix
+    else if node.type == "git"
+    then
+      fetchTarball {
+        url = node.url;
+        sha256 = node.narHash;
+      }
+    else throw "Unsupported lock type '${node.type}' for ${inputName}";
+
+  flakeCompatSrc = fetchInput "flake-compat";
+in
+  (import flakeCompatSrc {
+    src = ./.;
+    copySourceTreeToStore = false;
+    useBuiltinsFetchTree = true;
+  }).defaultNix
